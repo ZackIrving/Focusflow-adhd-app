631d30e65da43e253e423a4a5b47321610662510
ZackIrving <Zackirving96@gmail.com>
2026-06-15 10:00:07 -0500

    Add PWA install support

---
+++ public/service-worker.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'focusflow-cache-v1'
+
+const urlsToCache = [
+    '/',
+    '/manifest.json',
+    '/icon-192.png',
+    '/icon-512.png',
+]
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+    )
+})
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) =>
+            Promise.all(
+                cacheNames
+                    .filter((cacheName) => cacheName !== CACHE_NAME)
+                    .map((cacheName) => caches.delete(cacheName))
+            )
+        )
+    )
+})
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+            return cachedResponse || fetch(event.request)
+        })
+    )
+})
